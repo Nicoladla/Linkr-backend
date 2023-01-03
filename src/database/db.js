4980154870e432a45203fa60ba817faccb9e2a59
Nicolas Araújo <nicolasaraujo@gmail.com>
2023-01-02 22:18:57 -0300

    feat: conexão com a banco de dados

---
+++ src/database/db.js
@@ -0,0 +1,16 @@
+import pg from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+const { Pool } = pg;
+
+const configDatabase = {
+  connectionString: process.env.DATABASE_URL,
+};
+
+if (process.env.MODE === "prod") configDatabase.ssl = true;
+
+const connection = new Pool(configDatabase);
+console.log("Connected database.");
+
+export default connection;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+
+import indexRouter from "./routers/indexRouters.js";
+
+dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-const port= process.env.PORT || 5000
-app.listen(port, () => console.log(`App running on port ${port}`))
+app.use(indexRouter);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`App running on port: ${port}`));
