9aa536d38f9821919d8ac3162b049df67f29d7ef
manuelamp <manuella.m.p182@gmail.com>
2023-01-05 17:57:50 -0300

    feat: organization and validation

--- src/controllers/likesController.js
+++ src/controllers/likesController.js
@@ -41,3 +41,33 @@ export async function deleteLike(req, res) {
     }
 
 }
+
+export async function getUsers(req, res) {
+    
+    const {username} = req.query;
+
+    if(username){
+        try {
+            const usernameLower = username.toLowerCase();
+
+            const users = await searchUsers(usernameLower);
+            return res.status(200).send(users.rows);
+        } catch (error) {
+            return res.status(500).send({message: error.message});
+        }
+    }
+
+}
+
+export async function getPostsByUser(req, res){
+
+    const { id } = req.params;
+
+    try {
+        const posts = await postsByUser(id);
+        return res.status(200).send(posts.rows);
+    } catch (error) {
+        return res.status(500).send({message: error.message});
+    }
+
+}
